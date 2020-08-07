e30086c34d9b099399c850d86ec7999abe9b1f46
王浩 <wanghao@mail.fw-iot.com>
2020-08-08 00:50:40 +0800

    初始化项目

---
+++ pages/Login/index.vue
@@ -0,0 +1,218 @@
+<template>
+	<view class="container">
+		<view class="left-bottom-sign"></view>
+		<view class="back-btn yticon icon-zuojiantou-up" @click="() => uni.navigateBack()"></view>
+		<view class="right-top-sign"></view>
+		<!-- 设置白色背景防止软键盘把下部绝对定位元素顶上来盖住输入框等 -->
+		<view class="wrapper">
+			<view class="left-top-sign">LOGIN</view>
+			<view class="welcome">
+				欢迎回来！
+			</view>
+			<view class="input-content">
+				<form @submit="toLogin">
+					<view class="input-item">
+						<text class="tit">账号</text>
+						<input 
+							type="text" 
+							:value="username"
+							data-key="username"
+							placeholder="请输入账号"
+							@input="inputChange"
+						/>
+					</view>
+					<view class="input-item">
+						<text class="tit">密码</text>
+						<input 
+							type="password"
+							:value="password"
+							placeholder="请输入密码"
+							password
+							data-key="password"
+							@input="inputChange"
+						/>
+					</view>
+					<button form-type="submit" class="confirm-btn">登录</button>
+				</form>
+			</view>
+			<view class="forget-section">
+				忘记密码?
+			</view>
+		</view>
+		<view class="register-section">
+			还没有账号?
+			<text @click="toRegist">马上注册</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	import { mapMutations } from 'vuex';
+	
+	export default{
+		data(){
+			return {
+				username: '',
+				password: '',
+				mode: ''
+			}
+		},
+		onLoad(option){
+			this.mode = option.mode;
+			this.username = '123';
+			this.password = '1234'
+		},
+		methods: {
+			...mapMutations(['login']),
+			inputChange(e){
+				const key = e.currentTarget.dataset.key;
+				this[key] = e.detail.value;
+			},
+			toRegist(){
+				this.$api.msg('去注册');
+			},
+			toLogin(){
+				const {username, password, mode} = this;
+				uni.switchTab({url: '/pages/index/index'})
+			}
+		},
+
+	}
+</script>
+
+<style lang='scss'>
+	page{
+		background: #fff;
+	}
+	.container{
+		padding-top: 115px;
+		position:relative;
+		width: 100vw;
+		height: 100vh;
+		overflow: hidden;
+		background: #fff;
+	}
+	.wrapper{
+		position:relative;
+		z-index: 90;
+		background: #fff;
+		padding-bottom: 40upx;
+	}
+	.back-btn{
+		position:absolute;
+		left: 40upx;
+		z-index: 9999;
+		padding-top: var(--status-bar-height);
+		top: 40upx;
+		font-size: 40upx;
+		color: $font-color-dark;
+	}
+	.left-top-sign{
+		font-size: 120upx;
+		color: $page-color-base;
+		position:relative;
+		left: -16upx;
+	}
+	.right-top-sign{
+		position:absolute;
+		top: 80upx;
+		right: -30upx;
+		z-index: 95;
+		&:before, &:after{
+			display:block;
+			content:"";
+			width: 400upx;
+			height: 80upx;
+			background: #b4f3e2;
+		}
+		&:before{
+			transform: rotate(50deg);
+			border-radius: 0 50px 0 0;
+		}
+		&:after{
+			position: absolute;
+			right: -198upx;
+			top: 0;
+			transform: rotate(-50deg);
+			border-radius: 50px 0 0 0;
+			/* background: pink; */
+		}
+	}
+	.left-bottom-sign{
+		position:absolute;
+		left: -270upx;
+		bottom: -320upx;
+		border: 100upx solid #d0d1fd;
+		border-radius: 50%;
+		padding: 180upx;
+	}
+	.welcome{
+		position:relative;
+		left: 50upx;
+		top: -90upx;
+		font-size: 46upx;
+		color: #555;
+		text-shadow: 1px 0px 1px rgba(0,0,0,.3);
+	}
+	.input-content{
+		padding: 0 60upx;
+	}
+	.input-item{
+		display:flex;
+		flex-direction: column;
+		align-items:flex-start;
+		justify-content: center;
+		padding: 0 30upx;
+		background:$page-color-light;
+		height: 120upx;
+		border-radius: 4px;
+		margin-bottom: 50upx;
+		&:last-child{
+			margin-bottom: 0;
+		}
+		.tit{
+			height: 50upx;
+			line-height: 56upx;
+			font-size: $font-size-sm+2upx;
+			color: $font-color-base;
+		}
+		input{
+			height: 60upx;
+			font-size: $font-size-base + 2upx;
+			color: $font-color-dark;
+			width: 100%;
+		}	
+	}
+
+	.confirm-btn{
+		width: 630upx;
+		height: 76upx;
+		line-height: 76upx;
+		border-radius: 50px;
+		margin-top: 70upx;
+		background: $uni-color-primary;
+		color: #fff;
+		font-size: $font-size-base;
+		&:after{
+			border-radius: 100px;
+		}
+	}
+	.forget-section{
+		font-size: $font-size-sm+2upx;
+		color: $base-color;
+		text-align: center;
+		margin-top: 40upx;
+	}
+	.register-section{
+		position:absolute;
+		left: 0;
+		bottom: 50upx;
+		width: 100%;
+		font-size: $font-size-sm+2upx;
+		text-align: center;
+		text{
+			color: $base-color;
+			margin-left: 10upx;
+		}
+	}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+function resolve(dir) {
+	return path.join(__dirname, '.', dir) // 这里采用一个点，因为vue.config.js文件和package.json文件都在同一个目录下，即根目录下
+}
+
+module.exports = {
+	configureWebpack: {
+		resolve: {
+			alias: {
+				'@static': resolve('static'),
+				'@utils': resolve('common/utils.js')
+			}
+		},
+	}
+}
